6ae8a2d1c541edb7ec2dfe7ce10d71806b13b996
Ivan Drugov <vanyadr@yandex.ru>
2026-02-06 12:07:42 +0300

    feature: add menu slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+import Swiper from "../libraries/swiper/swiper.min.mjs";
+import Navigation from "../libraries/swiper/modules/navigation.min.mjs";
+import Autoplay from "../libraries/swiper/modules/autoplay.min.mjs";
+
 // Get elements from DOM
 const burger = document.querySelector(".header-nav__burger");
 const closer = document.querySelector(".header-nav__closer");
@@ -6,6 +10,8 @@ const menu = document.querySelector(".header-nav");
 // Start scrips on DOM load
 document.addEventListener("DOMContentLoaded", () => {
    toggleBurger(burger, closer);
+   initSliderMenu();
+   initSliderEvents();
 });
 
 //Functions
@@ -15,5 +21,69 @@ const toggleBurger = (openEl, closeEl) => {
    });
    closeEl.addEventListener("click", () => {
       menu.classList.remove("opened");
+   });
+};
+const initSliderMenu = () => {
+   const swiper = new Swiper(".swiper-menu", {
+      modules: [Autoplay],
+      slidesPerView: 3.5,
+      speed: 400,
+      spaceBetween: 8,
+      autoplay: {
+         delay: 3000,
+         pauseOnMouseEnter: true,
+      },
+      breakpoints: {
+         769: {
+            slidesPerView: 5,
+            spaceBetween: 8,
+         },
+         1025: {
+            slidesPerView: 7,
+            spaceBetween: 16,
+         },
+         1401: {
+            slidesPerView: 7,
+            spaceBetween: 24,
+         },
+         1801: {
+            slidesPerView: 9,
+            spaceBetween: 24,
+         },
+      },
+      loop: true,
+      centeredSlides: true,
+   });
+};
+const initSliderEvents = () => {
+   const swiper = new Swiper(".swiper-events", {
+      modules: [Navigation, Autoplay],
+      slidesPerView: 2,
+      speed: 400,
+      spaceBetween: 16,
+      autoplay: {
+         delay: 3000,
+         pauseOnMouseEnter: true,
+      },
+      navigation: {
+         nextEl: ".swiper-button-next",
+         prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+         769: {
+            slidesPerView: 3.3,
+            spaceBetween: 16,
+         },
+         1025: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+         },
+         1801: {
+            slidesPerView: 5.5,
+            spaceBetween: 24,
+         },
+      },
+      loop: true,
+      centeredSlides: true,
    });
 };
